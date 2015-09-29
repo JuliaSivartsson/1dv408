5c46482dbf8a3082bc8d71b8166382fb23e86114
Julia Sivartsson <jsigc09@student.lnu.se>
2015-09-29 13:48:08 +0200

    Update DatabaseConnection.php

--- Assignment4/model/DAL/DatabaseConnection.php
+++ Assignment4/model/DAL/DatabaseConnection.php
@@ -5,9 +5,9 @@ namespace model\dal;
 class DatabaseConnection {
 
     private static $Dsn = 'localhost';
-    private static $Database = 'assignment4';
-    private static $User = 'root';
-    private static $Password = '';
+    private static $Database = '****';
+    private static $User = '****';
+    private static $Password = '****';
 
     public function SetupDatabase() {
         return new \PDO('mysql:host=' . self::$Dsn . ';dbname=' . self::$Database . ';', self::$User, self::$Password, array(\PDO::FETCH_OBJ));
