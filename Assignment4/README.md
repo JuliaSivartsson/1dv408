17f04695483b378e1d061c6fd6e31c4e2592e63c
Julia Sivartsson <jsigc09@student.lnu.se>
2015-09-29 13:32:08 +0200

    test

--- Assignment4/README.md
+++
@@ -1,4 +0,0 @@
-# Login_1DV608
-Repository for 1DV608 assignment 2
-
-This web application demonstrates a MVC implementation with PHP of a login system.
--- Assignment4/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Include needed file
-require_once('config.php');
-
-//Create object of the controller
-$controller = new \controller\LoginController();
-$controller->render();
-
